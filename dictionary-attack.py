c1c69e8a3d5866af7c8289b9dde22448c57f10dd
ethicnology <ethicnology@pm.me>
2020-05-10 17:06:06 +0200

    dictionary attack wip

--- dictionary-attack.py
+++ dictionary-attack.py
@@ -1,5 +1,14 @@
 import sys
 import hashlib
+import string
+from itertools import chain, product
+
+
+def bruteforce(charset, maxlength):
+    return (''.join(candidate)
+            for candidate in chain.from_iterable(product(charset, repeat=i)
+                                                 for i in range(1, maxlength + 1)))
+
 
 # Passwords in memory
 with open("passwords.txt", "r") as f:
@@ -9,7 +18,7 @@ dictionary = set()
 for line in sys.stdin:
     dictionary.add(line.strip())
 
-# Is any animal match ?
+# dictionary word
 counter = 0
 success = 0
 for word in dictionary:
@@ -23,17 +32,74 @@ for word in dictionary:
     sys.stdout.write("Attempt : {}, Success : {}\r".format(
         counter, success))
 
-# Is any concat animals match ?
+# dictionary word + word
 for word1 in dictionary:
     for word2 in dictionary:
-        animal_concat = word1 + word2
+        word_concat = word1 + word2
         dictionary_hash = hashlib.md5(
-            animal_concat.encode('utf-8')).hexdigest()
+            word_concat.encode('utf-8')).hexdigest()
         counter += 1
         for password in passwords:
             if password == dictionary_hash:
                 success += 1
                 sys.stdout.write("The password = {} match hash : {} after {} attempts\n".format(
-                    animal_concat, password, counter))
+                    word_concat, password, counter))
+    sys.stdout.write("Attempt : {}, Success : {}\r".format(
+        counter, success))
+
+# dictionary word + digit
+for word in dictionary:
+    for randomDigit in bruteforce(string.digits, 3):
+        counter += 1
+        attempt = word+randomDigit
+        wordDigit_hash = hashlib.md5(attempt.encode('utf-8')).hexdigest()
+        for password in passwords:
+            if password == wordDigit_hash:
+                success += 1
+                sys.stdout.write("The password = {} match hash : {} after {} attempts\n".format(
+                    attempt, password, counter))
+        sys.stdout.write("Attempt : {}, Success : {}\r".format(
+            counter, success))
+
+# digit + dictionary word
+for word in dictionary:
+    for randomDigit in bruteforce(string.digits, 3):
+        counter += 1
+        attempt = randomDigit + word
+        wordDigit_hash = hashlib.md5(attempt.encode('utf-8')).hexdigest()
+        for password in passwords:
+            if password == wordDigit_hash:
+                success += 1
+                sys.stdout.write("The password = {} match hash : {} after {} attempts\n".format(
+                    attempt, password, counter))
+        sys.stdout.write("Attempt : {}, Success : {}\r".format(
+            counter, success))
+
+# dictionary reversed word x2
+for word in dictionary:
+    reverseWord = word[::-1]
+    doubleReverse = reverseWord + reverseWord
+    dictionary_hash = hashlib.md5(doubleReverse.encode('utf-8')).hexdigest()
+    counter += 1
+    for password in passwords:
+        if password == dictionary_hash:
+            success += 1
+            sys.stdout.write("The password = {} match hash : {} after {} attempts\n".format(
+                doubleReverse, password, counter))
+    sys.stdout.write("Attempt : {}, Success : {}\r".format(
+        counter, success))
+
+# dictionary word + reversed word
+for word1 in dictionary:
+    for word2 in dictionary:
+        word_concat = word1 + word2[::-1]
+        dictionary_hash = hashlib.md5(
+            word_concat.encode('utf-8')).hexdigest()
+        counter += 1
+        for password in passwords:
+            if password == dictionary_hash:
+                success += 1
+                sys.stdout.write("The password = {} match hash : {} after {} attempts\n".format(
+                    word_concat, password, counter))
     sys.stdout.write("Attempt : {}, Success : {}\r".format(
         counter, success))
